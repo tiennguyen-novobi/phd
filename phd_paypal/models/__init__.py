8f050d487afad9d7a5b551c586fd32cd5875b953
Tien Nguyen <tien.nguyen@novobi.com>
2022-01-17 10:26:52 +0700

    Test

--- phd_paypal/models/__init__.py
+++ phd_paypal/models/__init__.py
@@ -1,6 +1,6 @@
+from . import account_move
 from . import paypal_request
 from . import res_config_settings
 from . import paypal_transaction
 from . import res_company
 from . import request_log
-from . import account_move
